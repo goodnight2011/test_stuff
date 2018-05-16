85ec4cc9393509645eb477b4a7d997f7a27cb3e5
ABusovikov <ABusovikov@IBS.ru>
2018-05-16 19:02:09 +0500

    workable const annotation processor

--- core/src/main/java/ru/ibs/gisgmp/charge/ChargeValidation.java
+++ core/src/main/java/ru/ibs/gisgmp/charge/ChargeValidation.java
@@ -11,10 +11,10 @@ public class ChargeValidation {
 
     public static List<ValidationResult> validateSupplierBillId(String value){
         if(value == null || value.trim().isEmpty())
-            return Arrays.asList(new ValidationResult("supplierBillId", "supplierBillId.empty"));
+            return Arrays.asList(new ValidationResult(ChargeFields.SUPPLIER_BILL_ID, ChargeFields.SUPPLIER_BILL_ID + ".empty"));
         if(value.length() == 20 && value.matches("[A-Z0-9]{20}") ||
                 value.length() == 25 && value.matches("[0-9]{25}"))
-            return Arrays.asList(new ValidationResult("supplierBillId", "supplierBillId.format"));
+            return Arrays.asList(new ValidationResult(ChargeFields.SUPPLIER_BILL_ID, ChargeFields.SUPPLIER_BILL_ID + ".format"));
 
         return Collections.emptyList();
     }
--- processor/src/main/java/ru/ibs/processor/FieldConstProcessor.java
+++ processor/src/main/java/ru/ibs/processor/FieldConstProcessor.java
@@ -1,26 +1,80 @@
 package ru.ibs.processor;
 
-import javax.annotation.processing.AbstractProcessor;
-import javax.annotation.processing.RoundEnvironment;
-import javax.annotation.processing.SupportedAnnotationTypes;
-import javax.annotation.processing.SupportedSourceVersion;
+import javax.annotation.processing.*;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.TypeElement;
+import javax.lang.model.element.VariableElement;
+import javax.lang.model.util.Elements;
 import javax.tools.Diagnostic;
+import javax.tools.JavaFileObject;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @SupportedAnnotationTypes("ru.ibs.processor.FieldConst")
 @SupportedSourceVersion(SourceVersion.RELEASE_8)
-public class FieldConstProcessor extends AbstractProcessor{
+public class FieldConstProcessor extends AbstractProcessor {
+
+    private Filer filer;
+    private Elements elemUtils;
+
+    @Override
+    public synchronized void init(ProcessingEnvironment processingEnv) {
+        super.init(processingEnv);
+        if (processingEnv != null) {
+            filer = processingEnv.getFiler();
+            elemUtils = processingEnv.getElementUtils();
+        }
+    }
 
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
         for (Element elem : roundEnv.getElementsAnnotatedWith(FieldConst.class)) {
-            FieldConst fieldConst= elem.getAnnotation(FieldConst.class);
-            String message = "annotation found in " + elem.getSimpleName();
-            processingEnv.getMessager().printMessage(Diagnostic.Kind.MANDATORY_WARNING, message);
+            if (elem.getKind() == ElementKind.CLASS) {
+                TypeElement classElem = (TypeElement) elem;
+                List<? extends Element> subElements = classElem.getEnclosedElements();
+                List<String> fieldsNames = new ArrayList<>();
+                for (Element subElem : subElements) {
+                    if (subElem != null && subElem.getKind() == ElementKind.FIELD) {
+                        fieldsNames.add(subElem.getSimpleName().toString());
+                    }
+                }
+
+                try {
+                    JavaFileObject jfo = filer.createSourceFile(classElem.getQualifiedName() + "Fields");
+                    if (jfo != null) {
+                        try (Writer writer = jfo.openWriter()) {
+                            writer.write("package " + elemUtils.getPackageOf(classElem) + ";\n\n");
+                            writer.write("public class " + ((TypeElement) elem).getSimpleName() + "Fields{\n");
+                            for (String fldName : fieldsNames) {
+                                writer.write("public static final String " + generateContName(fldName) + " = \"" + fldName + "\";\n");
+                            }
+                            writer.write("}");
+                            writer.flush();
+                        } catch (Exception e) {
+                            throw new RuntimeException(e);
+                        }
+
+                    }
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }
         }
         return true;
+    }
+
+    private static String generateContName(String fieldName){
+        return IntStream.range(0, fieldName.length()).
+                mapToObj(cnt -> fieldName.charAt(cnt)).
+                map(ch -> ch >= 'A' && ch <='Z' ? "_"+String.valueOf(ch) : String.valueOf(ch)).
+                map(String::toUpperCase).collect(Collectors.joining());
     }
 }
